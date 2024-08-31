0bfd886036090d47dd57e401942734dedf7e43f8
Nintonito <110001155+Nintonito@users.noreply.github.com>
2024-08-31 16:47:34 -0400

    Outlined CNC File Format
    Outlined the cnc file format to include a header block that defines certain parameters on a per-file basis, giving tests more flexibility with how they format their data.  Still just an outline though.

--- Framework/storage.h
+++ Framework/storage.h
@@ -4,8 +4,8 @@
  * Program Name: CnC Common Headers
  * File Name: storage.h
  * Date Created: February 4, 2024
- * Date Updated: February 5, 2024
- * Version: 0.1
+ * Date Updated: August 31, 2024
+ * Version: 0.2
  * Purpose: Provides a struct for storing results and functions for file logging
  */
 #include <stdio.h>
@@ -19,6 +19,15 @@ struct resultEntry
 };
 
 /*
+ * Writes the header of the csv, which defines table names and certain formatting rules.
+ */
+
+int write_CNC_header()
+{
+
+}
+
+/*
  * Writes the stored list of results to a csv file
  * @Param TestName: a char array representing the name of the test.
  * @Param resultList: an array of resultEntry structs.
@@ -26,7 +35,7 @@ struct resultEntry
  * @Return: 0 if successful
  */
 
-int write_CSV(char testName[], struct resultEntry resultList[], int resultCount)
+int write_CNC_contents(char testName[], struct resultEntry resultList[], int resultCount)
 {
     return 0;
 }
