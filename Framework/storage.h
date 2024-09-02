138a063debad1490ce7a56ab7e1df9c54dc4df94
Nintonito <110001155+Nintonito@users.noreply.github.com>
2024-09-02 13:36:17 -0400

    Implemented .cnc File Write
    File writing is now supported.  File name handling is still unfinished and currently the passed-in string must contain the full file name or it will fail to execute.

--- Framework/storage.h
+++ Framework/storage.h
@@ -4,40 +4,56 @@
  * Program Name: CnC Common Headers
  * File Name: storage.h
  * Date Created: February 4, 2024
- * Date Updated: August 31, 2024
+ * Date Updated: September 2, 2024
  * Version: 0.2
  * Purpose: Provides a struct for storing results and functions for file logging
  */
 #include <stdio.h>
 #include <stdint.h>
 
-struct headerEntry
+struct CnCData
 {
-    char headerTag[13] = "Chips&Cheese";
+    char testName[255];
+    uint32_t resultCount;
     uint16_t columns;
-    //uint32_t rows;
+    uint64_t resultList[];
 };
 
 
-/*
- * Writes the header of the csv, which defines table names and certain formatting rules.
- */
-
-int write_CNC_header(char testName[])
-{
-
-}
 
 /*
- * Writes the stored list of results to a csv file
+ * Writes the stored list of results to a csv file with the .cnc extension
  * @Param TestName: a char array representing the name of the test.
- * @Param resultList: an array of resultEntry structs.
+ * @Param resultList: an array of results.
  * @Param resultCount: The length of the resultList array passed in.
- * @Return: 0 if successful
+ * @Param columnCount: The number of columns per line
+ * @Return: 0 if successful, -1 for IO error
  */
 
-int write_CNC_contents(char testName[], uint64_t resultList[], int resultCount)
+int write_CNC(char testName[], uint64_t resultList[], int resultCount, uint16_t columnCount)
 {
+    //This block is unfinished and does not yet handle file extension or naming properly
+    FILE *file;
+    if((file = fopen(testName, "w")) == NULL) return -1;
+    uint16_t columnCursor = 0;
+
+    //Writes the resultList in csv format with columnCount enforcing the frequency of line breaks
+    for(int i = 0; i < resultCount; i++)
+    {
+        fwrite(&resultList[i], sizeof(uint64_t), 1, file);
+        if(columnCursor < columnCount)
+        {
+            fwrite(",", sizeof(char), 1, file);
+            columnCursor++;
+        }
+        else
+        {
+            fwrite("\n", sizeof(char), 1, file);
+            columnCursor = 0;
+        }
+    }
+
+    fclose(file);
     return 0;
 }
 #endif // STORAGE_H
