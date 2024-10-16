13971d6fb3caef1f3919f4193f1c3d44a4f2bd1e
Lillith Beatrix <lillithbeatrix@gmail.com>
2024-10-16 19:53:53 -0400

    Fix footprint issues for better platform independence

--- Framework/platformCode.h
+++ Framework/platformCode.h
@@ -13,6 +13,7 @@
 // MINGW (at least on debian) expects _aligned_malloc, while gcc and clang expect aligned_alloc
 #define aligned_alloc(sz, aln) _aligned_malloc(sz, aln)
 #include <windows.h>
+#include <pthread.h>
 int getThreadCount()
 {
     SYSTEM_INFO sysinfo;
@@ -20,14 +21,15 @@ int getThreadCount()
     return sysinfo.dwNumberOfProcessors;
 }
 
-int setAffinity(HANDLE thread, int proc)
+int setAffinity(pthread_t thread, int proc)
 {
+    HANDLE native_handle = pthread_gethandle(thread);
     // This isn't guaranteed to work on systems with more than 64 processors
     // To fix this we need to look into process groups
     DWORD mask = 1 << proc;
 
-    return SetProcessAffinityMask(
-        thread,
+    return !SetProcessAffinityMask(
+        native_handle,
         (DWORD_PTR)&mask
     );
 }
